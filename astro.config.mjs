8abfcabbcf2ed143a2bbad2819f72cb6afd7cdfe
Carlos Meijomil <carlos.meijo@gmail.com>
2024-06-06 20:13:00 +0200

    fin seccion 6, islas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tutorial-astro-web.netlify.app/"
-
+  site: "https://tutorial-astro-web.netlify.app/",
+  integrations: [preact()]
 });
